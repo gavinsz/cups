48c5987b23e97015f0aadaa929f2216bb2733a1a
gavin <freeswitch@126.com>
2019-08-11 12:07:55 +0800

    add backendSNMPGetState

--- backend/printer_status.c
+++ backend/printer_status.c
@@ -18,47 +18,40 @@
  * 'main()' - Show the supplies state of a printer.
  */
 
-int					/* O - Exit status */
+int					        /* O - Exit status */
 main(int  argc,				/* I - Number of command-line args */
      char *argv[])			/* I - Command-line arguments */
 {
-  http_addrlist_t	*host;		/* Host addresses */
-  int			snmp_fd;	/* SNMP socket */
-  int			page_count,	/* Current page count */
-			printer_state;	/* Current printer state */
+    http_addrlist_t	*host;		/* Host addresses */
+    int			snmp_fd;	/* SNMP socket */
+    int			page_count,	/* Current page count */
+            printer_state;	/* Current printer state */
+    char *new_state_str = NULL;
+    char *new_supply_str = NULL;
+    char marker_levels[CUPS_MAX_SUPPLIES * 4] = {0};
 
-  if (argc != 2)
-  {
-    puts("Usage: testsupplies ip-or-hostname");
-    return (1);
-  }
+    if (argc != 2){
+        puts("Usage: printer_status ip-or-hostname");
+        return (1);
+    }
 
-  if ((host = httpAddrGetList(argv[1], AF_UNSPEC, "9100")) == NULL)
-  {
-    perror(argv[1]);
-    return (1);
-  }
+    if ((host = httpAddrGetList(argv[1], AF_UNSPEC, "9100")) == NULL){
+        perror("httpAddrGetList failed");
+        return (1);
+    }
 
-  if ((snmp_fd = _cupsSNMPOpen(host->addr.addr.sa_family)) < 0)
-  {
-    perror(argv[1]);
-    return (1);
-  }
+    if ((snmp_fd = _cupsSNMPOpen(host->addr.addr.sa_family)) < 0){
+        perror("_cupsSNMPOpen failed");
+        return (1);
+    }
 
-//  for (;;)
-//  {
-	  char *new_state_str = NULL;
-	  char *new_supply_str = NULL;
-	  char	marker_levels[CUPS_MAX_SUPPLIES * 4] = {0};
-    fputs("backendSNMPSupplies: ", stdout);
-
-    if (backendSNMPGetState(snmp_fd, &(host->addr),
+    if (0 != backendSNMPGetState(snmp_fd, &(host->addr),
                             &page_count,
                             &printer_state,
                             &new_supply_str,
                             &new_state_str,
                             marker_levels)){
-        puts("FAIL");
+        printf("backendSNMPGetState failed");
         return (1);
     }
 
@@ -75,7 +68,5 @@ main(int  argc,				/* I - Number of command-line args */
             new_state_str,
             marker_levels);
 
-//    sleep(5);
-//  }
     return 0;
 }
